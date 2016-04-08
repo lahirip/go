63d271b89b6a2a4dbd2ac2d2d8ad7096aa2c86a2
pallab <pallab.lahiri@gmail.com>
2016-04-07 23:34:43 -0700

    Added DB based login

--- src/github.com/lahirip/gologin/login.go
+++ src/github.com/lahirip/gologin/login.go
@@ -5,6 +5,8 @@ import (
     "html/template"
     "log"
     "net/http"
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
 )
 
 
@@ -21,8 +23,42 @@ func login(w http.ResponseWriter, r *http.Request) {
     } else {
         r.ParseForm()
         // logic part of log in
-        fmt.Println("username:", r.Form["username"])
-        fmt.Println("password:", r.Form["password"])
+        username := string(r.Form["username"][0])
+	password := string(r.Form["password"][0])
+	fmt.Println("username:", r.Form["username"])
+        fmt.Println("password:", r.Form["password"])	
+
+	check := validateLogin(username, password) 
+        fmt.Println("check:", check)
+	if check == 1 {
+            fmt.Println("here...")
+	    t, _ := template.ParseFiles("welcome.html")
+            t.Execute(w, nil)
+	}  else {
+	    t, _ := template.ParseFiles("login.html")
+            t.Execute(w, nil)
+        }
+    }
+}
+
+func validateLogin(username , password string) int {
+    //TCP on a remote host, e.g. Amazon RDS:
+    //db, err := sql.Open("mysql", id:password@tcp(your-amazonaws-uri.com:3306)/dbname)    
+
+    db, err := sql.Open("mysql", "root:alap@/test?charset=utf8")
+    checkErr(err)
+
+    // query
+   cnt := 0
+   err = db.QueryRow("SELECT count(*) FROM userlogin WHERE username = ? and password= ?", username, password).Scan(&cnt)
+   checkErr(err)
+    
+   return cnt
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
     }
 }
 
